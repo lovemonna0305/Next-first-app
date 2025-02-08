00cc4a86d6a2351bf73fbb02cf9acc413c537c30
lovemonna0305 <lovemonna0305@gmail.com>
2025-02-08 17:21:04 -0500

    change onClick function

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   };
 
 
-  
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -99,7 +99,7 @@ export default function Home() {
 
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          onClick={copyToClipboard}
+          onClick={() => copyToClipboard}
           rel="noopener noreferrer"
         >
           {'copyToClipboard'}
