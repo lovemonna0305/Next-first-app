022f5220bafb5e8277a29c470f1cfeca5ae9925c
lovemonna0305 <lovemonna0305@gmail.com>
2025-02-08 17:23:29 -0500

    22

--- app/page.tsx
+++ app/page.tsx
@@ -99,7 +99,7 @@ export default function Home() {
 
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          onClick={() => copyToClipboard}
+          onClick={() => {(copyToClipboard('copy Text'))}}
           rel="noopener noreferrer"
         >
           {'copyToClipboard'}
