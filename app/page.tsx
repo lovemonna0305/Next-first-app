a9c8fa42f80c6634fe7de97042f534c645e0c2cf
lovemonna0305 <lovemonna0305@gmail.com>
2025-02-08 17:18:39 -0500

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
 import Image from "next/image";
 
 export default function Home() {
+
+
+  const copyToClipboard = async (text) => {
+    console.log(text)
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log("text clipboard success");
+    } catch (err) {
+      console.log("text clipboard failed");
+      console.error("Failed to copy text: ", err);
+    }
+  };
+
+
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -80,20 +95,14 @@ export default function Home() {
           />
           Examples
         </a>
+
+
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
+          onClick={copyToClipboard}
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          {'copyToClipboard'}
         </a>
       </footer>
     </div>
